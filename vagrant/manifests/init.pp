c25ffa7cab7a3d7ec82db3cee16b23865e595db9
Juuso Puroila <jpuroila@gmail.com>
2017-05-17 07:40:19 +0000

    Merge portforward with vagrant-module

--- vagrant/manifests/init.pp
+++ vagrant/manifests/init.pp
@@ -31,4 +31,28 @@ class vagrant {
 		path => "/bin/:/usr/bin/:/sbin/:/usr/sbin/",
 		notify =>Package['virtualbox-5.0'],
 	}
-}
+	package{'apache2':
+		ensure =>installed,
+		allowcdrom =>true,
+	}
+	file{'/etc/apache2/ports.conf':
+		content =>template('portforward/ports.conf.erb'),
+		notify =>Service['apache2'],
+		require =>Package['apache2'],
+	}
+	service{'apache2':
+		ensure =>true,
+		enable =>true,
+		require =>Package['apache2'],
+		provider =>systemd,
+	}
+	exec{"sudo a2enmod proxy":
+		path => "/bin/:/usr/bin/:/sbin/:/usr/sbin/",
+		require =>Package['apache2'],
+		notify =>Service['apache2'],
+	}
+	exec{"sudo a2enmod proxy_http":
+		path => "/bin/:/usr/bin/:/sbin/:/usr/sbin/",
+                require =>Package['apache2'],
+                notify =>Service['apache2'],
+}}
